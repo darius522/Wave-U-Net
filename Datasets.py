3720d91aaf4a54974c8602b9676e30035d3d0478
Darius Petermann <dariuspetermann@darius-mac.home>
2019-11-24 22:36:42 +0100

    formatting tweaks

--- Datasets.py
+++ Datasets.py
@@ -375,55 +375,6 @@ def getSATB(database_path):
                 myfile.write(mix_audio)
             myfile.closed
 
-    # Go through stems
-    # for track in tracks:
-    #     # Skip track if mixture is already written, assuming this track is done already
-    #     track_path = track.path[:-4]
-    #     mix_path = track_path + "_mix.wav"
-    #     acc_path = track_path + "_accompaniment.wav"
-    #     if os.path.exists(mix_path):
-    #         print("WARNING: Skipping track " + mix_path + " since it exists already")
-
-    #         # Add paths and then skip
-    #         paths = {"mix" : mix_path, "accompaniment" : acc_path}
-    #         paths.update({key : track_path + "_" + key + ".wav" for key in ["bass", "drums", "other", "vocals"]})
-
-    #         samples.append(paths)
-
-    #         continue
-
-    #     rate = track.rate
-
-    #     # Go through each instrument
-    #     paths = dict()
-    #     stem_audio = dict()
-    #     for stem in ["bass", "drums", "other", "vocals"]:
-    #         path = track_path + "_" + stem + ".wav"
-    #         audio = track.targets[stem].audio
-    #         soundfile.write(path, audio, rate, "PCM_16")
-    #         stem_audio[stem] = audio
-    #         paths[stem] = path
-
-    #     # Add other instruments to form accompaniment
-    #     acc_audio = np.clip(sum([stem_audio[key] for key in list(stem_audio.keys()) if key != "vocals"]), -1.0, 1.0)
-    #     soundfile.write(acc_path, acc_audio, rate, "PCM_16")
-    #     paths["accompaniment"] = acc_path
-
-    #     # Create mixture
-    #     mix_audio = track.audio
-    #     soundfile.write(mix_path, mix_audio, rate, "PCM_16")
-    #     paths["mix"] = mix_path
-
-    #     diff_signal = np.abs(mix_audio - acc_audio - stem_audio["vocals"])
-    #     print("Maximum absolute deviation from source additivity constraint: " + str(np.max(diff_signal)))# Check if acc+vocals=mix
-    #     print("Mean absolute deviation from source additivity constraint:    " + str(np.mean(diff_signal)))
-
-    #     samples.append(paths)
-
-    # subsets.append(samples)
-
-    # return subsets
-
 def getCCMixter(xml_path):
     tree = etree.parse(xml_path)
     root = tree.getroot()
